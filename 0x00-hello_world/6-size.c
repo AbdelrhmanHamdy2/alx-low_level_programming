dc0fd9995e0dd790675ed9dd683812429c112d74
AbdelrhmanHamdy2 <hamdyabdo596@gmail.com>
2023-08-05 08:51:16 -0700

    first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int ll;
 	float f;
 
-printf ("Size of a char: %ld byte(s)\n",sizeof (c));	
-printf ("Size of an int: %d byte(s)\n",sizeof (d));
-printf ("Size of a long int: %d byte(s)\n",sizeof (ld));
-printf ("Size of a long long int: %d byte(s)\n",sizeof (ll));
-printf ("Size of a float: %d byte(s)\n",sizeof (f));
+printf("Size of a char: %ld byte(s)\n", sizeof (c));
+printf("Size of an int: %d byte(s)\n", sizeof (d));
+printf("Size of a long int: %d byte(s)\n", sizeof (ld));
+printf("Size of a long long int: %d byte(s)\n", sizeof (ll));
+printf("Size of a float: %d byte(s)\n", sizeof (f));
 return (0);
 }
